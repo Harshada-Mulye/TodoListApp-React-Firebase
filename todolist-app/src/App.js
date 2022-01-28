8f2a0329b432a7c86cdc34fb5a4e050def4ab445
Jayashree bathala <71385982+jayashreeiths@users.noreply.github.com>
2022-01-28 12:55:36 +0100

    connected to firebase and get the data

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,12 +1,25 @@
-
+import { useState,useEffect } from 'react';
+import { db } from "./backend/firebase-config";
+import {collection,getDocs,doc} from "firebase/firestore"
 import './App.css';
 
 function App() {
+  const [tasks,setTasks] = useState()
+  const tasksCollectionRef = collection(db, "todolist");
+  useEffect(() => 
+ 
+  {   
+   const getTasks = async () => {  
+        const data = await getDocs(tasksCollectionRef);   
+           setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+           console.log(data);
+            };
+           getTasks();  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-       hi
-      </header>
+      {tasks.map((task)=>{
+        return(<div>{task.taskName}</div>)
+        })}
     </div>
   );
 }
---
+++ todolist-app/src/backend/firebase-config.js
@@ -0,0 +1,8 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "@firebase/firestore"
+const firebaseConfig = require("./firebase-privatekey.json");
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db =getFirestore(app);
