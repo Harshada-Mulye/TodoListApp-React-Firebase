d4c8830464015462e4cbaa51805c098c221deb97
Harshada-Mulye <mulyeharshada@gmail.com>
2022-01-28 13:15:22 +0100

    fetch data

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,25 +1,23 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { db } from "./backend/firebase-config";
-import {collection,getDocs,doc} from "firebase/firestore"
-import './App.css';
+import { collection, getDocs, doc } from "firebase/firestore";
+import "./App.css";
 
 function App() {
-  const [tasks,setTasks] = useState()
+  const [tasks, setTasks] = useState([]);
   const tasksCollectionRef = collection(db, "todolist");
-  useEffect(() => 
- 
-  {   
-   const getTasks = async () => {  
-        const data = await getDocs(tasksCollectionRef);   
-           setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-           console.log(data);
-            };
-           getTasks();  }, []);
+  useEffect(() => {
+    const getTasks = async () => {
+      const data = await getDocs(tasksCollectionRef);
+      setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getTasks();
+  }, []);
   return (
     <div className="App">
-      {tasks.map((task)=>{
-        return(<div>{task.taskName}</div>)
-        })}
+      {tasks.map((task) => {
+        return <div>{task.taskName}</div>;
+      })}
     </div>
   );
 }
